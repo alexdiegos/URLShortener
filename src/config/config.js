d9567753bf2b7a7626f9fdbcde10af2cd6a403f1
Alexandre Silva <adiegossilva@gmail.com>
2022-08-18 16:09:35 -0300

    Update database

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 module.exports = {
     PORT: process.env.PORT || 8080,
-    DB_HOST: process.env.DB_HOST || 'urlshortenerdb.cdueplnz1rp4.us-east-1.rds.amazonaws.com',
+    DB_HOST: process.env.DB_HOST || 'awseb-e-hhw2spudmm-stack-awsebrdsdatabase-qgkdhqtld9d4.cdueplnz1rp4.us-east-1.rds.amazonaws.com',
     DB_USER: process.env.DB_USER || 'urlshortenerUsr',
     DB_PASS: process.env.DB_PASS || '358nCQ5ltZb0%ABwuI2o',
     DB_NAME: process.env.DB_NAME || 'urlshortenerdb',
